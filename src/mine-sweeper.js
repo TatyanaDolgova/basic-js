4e916015dfd6e5d8608f1f170668079584ee6469
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2023-04-17 15:45:53 +0500

    feat: complete mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,67 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let newMatrix = [];
+  for (let i = 0; i < matrix.length; i++) {
+    newMatrix[i] = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      newMatrix[i][j] = 0;
+      if (i > 0) {
+        let north = matrix[i - 1][j];
+        if (north === true) {
+          newMatrix[i][j] += 1;
+        }
+      }
+      if (i < matrix.length - 1) {
+        let south = matrix[i + 1][j];
+        if (south === true) {
+          newMatrix[i][j] += 1;
+        }
+      }
+      if (j > 0) {
+        let west = matrix[i][j - 1];
+        if (west === true) {
+          newMatrix[i][j] += 1;
+        }
+      }
+
+      if (j < matrix[i].length - 1) {
+        let east = matrix[i][j + 1];
+        if (east === true) {
+          newMatrix[i][j] += 1;
+        }
+      }
+      if (i > 0 && j < matrix[i].length - 1) {
+        let northEast = matrix[i - 1][j + 1];
+        if (northEast === true) {
+          newMatrix[i][j] += 1;
+        }
+      }
+
+      if (i > 0 && j > 0) {
+        let northWest = matrix[i - 1][j - 1];
+        if (northWest === true) {
+          newMatrix[i][j] += 1;
+        }
+      }
+
+      if (i < matrix.length - 1 && j > 0) {
+        let southWest = matrix[i + 1][j - 1];
+        if (southWest === true) {
+          newMatrix[i][j] += 1;
+        }
+      }
+
+      if (i < matrix.length - 1 && j < matrix[i].length - 1) {
+        let southEast = matrix[i + 1][j + 1];
+        if (southEast === true) {
+          newMatrix[i][j] += 1;
+        }
+      }
+    }
+  }
+  return newMatrix;
 }
 
 module.exports = {
